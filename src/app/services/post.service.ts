5f6a95b6d04de7d2f9cc6873b008075f8a04ffc4
kanpeipei <satosjhkkmp@gmail.com>
2020-05-02 20:05:33 +0900

    add:投稿作成ページ

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,9 +1,36 @@
+import { ErrorService } from './error.service';
+import { tap, catchError } from 'rxjs/operators';
+import { HttpService } from './http.service';
+import { Post } from './../models/index';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostService {
+  private url = 'post';
 
-  constructor() { }
+  constructor(private http: HttpService, private error: ErrorService) {}
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get(this.url).pipe(
+      tap(() => console.log('getPosts')),
+      catchError(this.error.handleError<any>('cannot getPosts'))
+    );
+  }
+
+  getPost(id: number): Observable<Post> {
+    return this.http.get(this.url, id).pipe(
+      tap(() => console.log('getPost')),
+      catchError(this.error.handleError<any>('cannot getPost'))
+    );
+  }
+
+  addPost(post: Post): Observable<Post> {
+    return this.http.post(this.url, post).pipe(
+      tap(() => console.log('addPost')),
+      catchError(this.error.handleError('cannot addPost'))
+    );
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,29 @@
+import { tap, catchError } from 'rxjs/operators';
+import { Task, Post } from './../models/index';
+import { Observable } from 'rxjs';
+import { ErrorService } from './error.service';
+import { HttpService } from './http.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private url = 'task';
 
-  constructor() { }
+  constructor(private http: HttpService, private error: ErrorService) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get(this.url).pipe(
+      tap(() => console.log('getTasks')),
+      catchError(this.error.handleError<any>('cannot getTasks'))
+    );
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post(this.url, task).pipe(
+      tap(() => console.log('addTask')),
+      catchError(this.error.handleError('cannot addTask'))
+    );
+  }
 }
