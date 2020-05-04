a6e9675496a186138f4345cf82ca0c946eaa4a8b
kanpeipei <satosjhkkmp@gmail.com>
2020-05-04 10:52:43 +0900

    add:投稿リスト

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,15 +1,35 @@
+import { TaskService } from './../../services/task.service';
+import { Post } from './../../models/index';
+import { PostService } from './../../services/post.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  public posts;
+  public tasks;
 
-  constructor() { }
+  constructor(
+    private postService: PostService,
+    private taskService: TaskService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.postService.getPosts().subscribe((posts) => {
+      this.posts = posts;
+    });
 
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
+
+    setTimeout(() => {
+      this.posts.forEach((value) => {
+        value['tasks'] = this.tasks.filter((task) => task.post_id === value.id);
+      });
+    }, 1000);
+  }
 }
