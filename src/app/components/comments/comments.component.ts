ffbf3ac26654e51b1ce370420b43d54ab71a1bfd
kanpeipei <satosjhkkmp@gmail.com>
2020-05-06 19:59:32 +0900

    add:コメント機能

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { CommentService } from './../../services/comment.service';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+  styleUrls: ['./comments.component.scss'],
 })
 export class CommentsComponent implements OnInit {
+  public comments;
+  private newComment;
 
-  constructor() { }
+  public commentContent: string;
+
+  @Input() post_id: number;
+
+  constructor(private commentService: CommentService) {}
 
   ngOnInit(): void {
+    this.commentService.getComments().subscribe((comments) => {
+      this.comments = comments.filter(
+        (value) => value['post_id'] === this.post_id
+      );
+    });
   }
 
+  create() {
+    this.newComment = {
+      content: this.commentContent,
+      user_id: 1,
+      post_id: this.post_id,
+    };
+    console.log(this.newComment);
+    this.commentService.addComment(this.newComment).subscribe((comment) => {
+      this.comments.push(comment);
+      this.commentContent = '';
+    });
+  }
 }
