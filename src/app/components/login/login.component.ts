ecbb8465acebb2ae13f989914325db4b256a808e
kanpeipei <satosjhkkmp@gmail.com>
2020-05-21 15:29:17 +0900

    add:マイページ

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -30,11 +30,8 @@ export class LoginComponent implements OnInit {
     if (!this.loginForm.invalid) {
       this.userService.login(this.loginForm.value).subscribe((res) => {
         console.log(res);
-        res['id'] = 1;
-        localStorage.setItem('current_user_id', res.id);
         localStorage.setItem('access_token', res.token);
         this.http.updateToken(localStorage.getItem('access_token'));
-        console.log(localStorage.getItem('access_token'));
         this.router.navigate(['list']);
       });
     }
