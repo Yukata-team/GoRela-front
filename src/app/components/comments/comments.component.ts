5e4f937862ddb2772c995d3e3a99ef3109c0b306
kanpeipei <satosjhkkmp@gmail.com>
2020-05-23 19:54:28 +0900

    add:ゲストユーザー

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Comment, Post } from './../../models/index';
 import { CommentService } from './../../services/comment.service';
 import { Component, OnInit, Input } from '@angular/core';
@@ -12,29 +13,33 @@ export class CommentsComponent implements OnInit {
   private newComment;
 
   public commentContent: string;
+  public currentUserId = sessionStorage.getItem('current_user_id');
 
   @Input() post: Post;
 
-  constructor(private commentService: CommentService) {}
+  constructor(
+    private commentService: CommentService,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
-    // this.commentService.getComments().subscribe((comments) => {
-    //   this.comments = comments.filter(
-    //     (value) => value['post_id'] === this.post_id
-    //   );
-    // });
     this.comments = this.post.comments;
     console.log(typeof(this.comments));
   }
 
   create() {
-    this.newComment = {
-      "content": this.commentContent
-    };
-    console.log(this.newComment);
-    this.commentService.addComment(this.newComment, this.post.id).subscribe((comment) => {
-      this.comments.unshift(comment);
-      this.commentContent = '';
-    });
+    if(!this.currentUserId){
+      this.router.navigate(['register']);
+    }
+    else{
+      this.newComment = {
+        "content": this.commentContent
+      };
+      console.log(this.newComment);
+      this.commentService.addComment(this.newComment, this.post.id).subscribe((comment) => {
+        this.comments.unshift(comment);
+        this.commentContent = '';
+      });
+    }
   }
 }
